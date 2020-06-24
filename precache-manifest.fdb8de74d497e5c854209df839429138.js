b4d68acb8cca8a60fe4de3d94678cccc8bc8ae54
Isaac Householder <isaachouseholder@gmail.com>
2020-06-23 19:23:25 -0700

    Updates

---
+++ precache-manifest.fdb8de74d497e5c854209df839429138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b72e952f33fc9967f9ca98e5adcf6c",
+    "url": "/Week10_HW001/index.html"
+  },
+  {
+    "revision": "c118062ee956e7fc6e5c",
+    "url": "/Week10_HW001/static/css/main.2457e642.chunk.css"
+  },
+  {
+    "revision": "5282a7ab19f12779166d",
+    "url": "/Week10_HW001/static/js/2.d02a2676.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Week10_HW001/static/js/2.d02a2676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c118062ee956e7fc6e5c",
+    "url": "/Week10_HW001/static/js/main.b97d0300.chunk.js"
+  },
+  {
+    "revision": "91bd3b11a489f2399e2f",
+    "url": "/Week10_HW001/static/js/runtime-main.f3a4afbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Week10_HW001/static/media/logo.5d5d9eef.svg"
+  }
+]);
